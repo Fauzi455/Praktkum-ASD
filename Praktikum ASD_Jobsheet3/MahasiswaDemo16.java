eced01e0f470e48ec6396e48f72873540c7096d4
Fauzi455 <fauzirhmn45@gmail.com>
2025-02-27 08:15:22 +0700

    Add files via upload

---
+++ Praktikum ASD_Jobsheet3/MahasiswaDemo16.java
@@ -0,0 +1,40 @@
+public class MahasiswaDemo16 {
+    public static void main(String[] args) {
+        Mahasiswa16[] arrayOfMahasiswa16 = new Mahasiswa16[3];
+        arrayOfMahasiswa16[0] = new Mahasiswa16();
+        arrayOfMahasiswa16[0].nim = "244107060033";
+        arrayOfMahasiswa16[0].nama = "AGNES TITANIA KINANTI";
+        arrayOfMahasiswa16[0].kelas = "SIB-1E";
+        arrayOfMahasiswa16[0].ipk = (float) 3.75;
+
+        arrayOfMahasiswa16[1] = new Mahasiswa16();
+        arrayOfMahasiswa16[1].nim = "2341720172";
+        arrayOfMahasiswa16[1].nama = "AHMAD MAULANA HAMZAH";
+        arrayOfMahasiswa16[1].kelas = "TI-2A";
+        arrayOfMahasiswa16[1].ipk = (float) 3.36;
+
+        arrayOfMahasiswa16[2] = new Mahasiswa16();
+        arrayOfMahasiswa16[2].nim = "244107023006";
+        arrayOfMahasiswa16[2].nama = "DIRHAMAWAN PUTRANTO";
+        arrayOfMahasiswa16[2].kelas = "TI-2E";
+        arrayOfMahasiswa16[2].ipk = (float) 3.80;
+
+
+        System.out.println("NIM     : " + arrayOfMahasiswa16[0].nim);
+        System.out.println("Nama    : " +arrayOfMahasiswa16[0].nama);
+        System.out.println("Kelas   : " + arrayOfMahasiswa16[0].kelas);
+        System.out.println("IPK     : " + arrayOfMahasiswa16[0].ipk);
+        System.out.println("--------------------------------------");
+        System.out.println("NIM     : " + arrayOfMahasiswa16[1].nim);
+        System.out.println("Nama    : " +arrayOfMahasiswa16[1].nama);
+        System.out.println("Kelas   : " + arrayOfMahasiswa16[1].kelas);
+        System.out.println("IPK     : " + arrayOfMahasiswa16[1].ipk);
+        System.out.println("--------------------------------------");
+        System.out.println("NIM     : " + arrayOfMahasiswa16[2].nim);
+        System.out.println("Nama    : " +arrayOfMahasiswa16[2].nama);
+        System.out.println("Kelas   : " + arrayOfMahasiswa16[2].kelas);
+        System.out.println("IPK     : " + arrayOfMahasiswa16[2].ipk);
+        System.out.println("--------------------------------------");
+    }
+    
+}
