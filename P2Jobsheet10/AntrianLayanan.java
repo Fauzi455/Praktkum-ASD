b3d6d68661899955a7bc15413ba0429ca11ecc1a
Fauzi455 <fauzirhmn45@gmail.com>
2025-05-21 20:01:50 +0700

    Add files via upload

---
+++ P2Jobsheet10/AntrianLayanan.java
@@ -0,0 +1,93 @@
+public class AntrianLayanan{
+Mahasiswa [] data;
+int front;
+int rear;
+int size;
+int max;
+
+public AntrianLayanan(int max){
+    this.max = max;
+    this.data = new Mahasiswa[max];
+    this.front = 0;
+    this.rear = -1;
+    size = 0;
+}
+
+public void tambahAntrian(Mahasiswa mhs){
+    if (IsFull()) {
+        System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+        return;
+    }
+    rear = (rear + 1) % max;
+    data[rear] = mhs;
+    size++;
+    System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+}
+
+public Mahasiswa layaniMahasiswa(){
+    if (IsEmpty()) {
+        System.out.println("Antrian kosong");
+        return null;
+    }
+    Mahasiswa mhs = data[front];
+    front = (front + 1) % max;
+    size--;
+    return mhs;
+}
+
+public void lihatTerdepan(){
+    if (IsEmpty()) {
+        System.out.println("Antrian kosong");
+    } else{
+        System.out.print("Mahasiswa terdepan: ");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        data[front].tampilkanData();
+    }
+}
+
+public void tampilkanSemua(){
+    if (IsEmpty()) {
+        System.out.println("Antrian kosong");
+        return;
+    }
+    System.out.println("Daftar Mahasiswa dalam Antrian:");
+    System.out.println("NIM - NAMA - PRODI - KELAS");
+    for (int i = 0; i < size; i++){
+        int index = (front + i) % max;
+        System.out.print((i + 1) + ". ");
+        data[index].tampilkanData();
+    }
+}
+
+public int getJumlahAntrian(){
+    return size;
+}
+
+public Mahasiswa LihatAkhir(){
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong, tidak ada data di posisi belakang.");
+        } else{
+            System.out.println("Mahasiswa paling belakang di antrian: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+        return data[rear];
+}
+
+
+public boolean IsEmpty(){
+    if (size == 0) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+public boolean IsFull(){
+    if (size == max) {
+        return true;
+    } else{
+        return false;
+    }
+}
+}
