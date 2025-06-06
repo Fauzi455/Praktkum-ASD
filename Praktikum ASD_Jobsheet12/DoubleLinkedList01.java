2eacb15db03da941e51629fc01473d1be7056fdb
Fauzi455 <fauzirhmn45@gmail.com>
2025-06-06 20:42:31 +0700

    Update DoubleLinkedList01.java

--- Praktikum ASD_Jobsheet12/DoubleLinkedList01.java
+++ Praktikum ASD_Jobsheet12/DoubleLinkedList01.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 public class DoubleLinkedList01 {
     Node01 head;
     Node01 tail;
+    int size;
 
 public DoubleLinkedList01(){
     head = null;
     tail = null;
+    size = 0;
 }
 
 public boolean isEmpty(){
@@ -21,6 +23,7 @@ public void addFirst(Mahasiswa01 data){
         head.prev = newNode;
         head = newNode;
     }
+    size++;
 }
 
 public void addLast(Mahasiswa01 data){
@@ -32,6 +35,7 @@ public void addLast(Mahasiswa01 data){
         newNode.prev = tail;
         tail = newNode;
     }
+    size++;
 }
 
 public void insertAfter(String keyNim, Mahasiswa01 data){
@@ -56,6 +60,7 @@ public void insertAfter(String keyNim, Mahasiswa01 data){
         current.next.prev = newNode;
         current.next = newNode;
     }
+    size++;
     System.out.println("Node berhasil disisipkan setelah NIM " + keyNim);
 }
 
@@ -77,11 +82,14 @@ public void removeFirst(){
         return;
     }
     if(head == tail){
+        System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah " + head.data.nama);  
         head = tail = null;
     } else{
+        System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah " + head.data.nama);  
         head = head.next;
         head.prev = null;
     }
+    size--;
 }
 
 public void removeLast(){
@@ -95,6 +103,7 @@ public void removeLast(){
        tail = tail.prev;
        tail.next = null;
     }
+    size--;
 }
 
 public Node01 search(String nim) {
@@ -120,4 +129,88 @@ public static Mahasiswa01 inputMahasiswa(Scanner scan){
     scan.nextLine();
     return new Mahasiswa01(nim, nama, kelas, ipk);
     }
+
+public void add(int index, Mahasiswa01 data){
+    if (index == 0) {
+        addFirst(data);
+    } else if (index == size) {
+        addLast(data);
+    } else {
+        Node01 newNode = new Node01(data);
+        Node01 current = head;
+    for (int i = 0; i < index; i++) {
+        current = current.next;
+    }
+        newNode.prev = current.prev;
+        newNode.next = current;
+        current.prev.next = newNode;
+        current.prev = newNode;
+        size++;
+        }
+    }
+
+public void removeAfter(String keyNim){
+    Node01 current = head;
+    while (current != null && !current.data.nim.equals(keyNim)) {
+        current = current.next;
+    }
+
+    if (current == null) {
+        System.out.println("Node dengan NIM " + keyNim + " tidak ditemukan.");
+        return;
+    }
+
+    Node01 remove = current.next;
+    if (remove == tail) {
+        tail = current;
+        current.next = null;
+    } else{
+        current.next = remove.next;
+        remove.next.prev = current;
+    }
+    size--;
+}
+
+public void remove(int index){
+    if (index == 0) {
+        removeFirst();
+    } else if (index == size - 1) {
+        removeLast();
+    } else {
+    Node01 current = head;
+    for (int i = 0; i < index; i++) {
+        current = current.next;
+    }
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+        size--;
+    }
+}
+
+public Mahasiswa01 getFirst(){
+    if (isEmpty()) {
+        System.out.println("Linked list masih kosong");
+        return null;
+    }
+    return head.data; 
+}
+
+public Mahasiswa01 getLast(){
+    if (isEmpty()) {
+        System.out.println("Linked list masih kosong");
+        return null;
+    }
+    return tail.data; 
+}
+public Mahasiswa01 getIndex(int index){
+    Node01 current = head;
+    for(int i = 0; i < index; i++){
+        current = current.next;
+    }
+    return current.data;
+}
+
+public int getSize(){
+    return size;
+}
 }
