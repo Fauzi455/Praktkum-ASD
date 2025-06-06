15a40421bd1a84c4c14386f10f462510182d4087
Fauzi455 <fauzirhmn45@gmail.com>
2025-06-06 20:44:10 +0700

    Add files via upload

--- Praktikum ASD_Jobsheet12/DLLMain.java
+++ Praktikum ASD_Jobsheet12/DLLMain.java
@@ -13,6 +13,12 @@ do{
     System.out.println("4. Hapus di akhir");
     System.out.println("5. Tampilkan data");
     System.out.println("6. Cari Mahasiswa berdasarkan NIM");
+    System.out.println("7. Sisipkan Data Baru");
+    System.out.println("8. Tambah Data di Indeks Tertentu");
+    System.out.println("9. Hapus Data Setelah Data Key ");
+    System.out.println("10. Hapus Data di Indeks Tertentu");
+    System.out.println("11. Tampilkan Data di Head, Tail dan Indeks Tertentu");
+    System.out.println("12. Jumlah Data pada DLL");
     System.out.println("0. Keluar");
     System.out.print("Pilih Menu: ");
     pilihan = scan.nextInt();
@@ -25,7 +31,7 @@ do{
         }
 
         case 2 ->{
-             Mahasiswa01 mhs = list.inputMahasiswa(scan);
+            Mahasiswa01 mhs = list.inputMahasiswa(scan);
             list.addLast(mhs);
         }
 
@@ -43,11 +49,55 @@ do{
                 System.out.println("Data tidak ditemukan");
             }
         }
-
+        case 7 -> {
+            System.out.print("Masukkan NIM yang dicari: ");
+            String key = scan.nextLine();
+            list.insertAfter(key, list.inputMahasiswa(scan));
+        }
+        case 8 -> {
+            System.out.print("Masukkan Index: ");
+            int index = scan.nextInt();
+            scan.nextLine();
+            list.add(index, list.inputMahasiswa(scan));
+        }
+        case 9 -> {
+            System.out.print("Masukkan NIM: ");
+            String key = scan.nextLine();
+            list.removeAfter(key);
+        }
+        case 10 -> {
+            System.out.print("Masukkan Indeks: ");
+            int index = scan.nextInt();
+            list.remove(index);
+        }
+        case 11 -> {
+            Mahasiswa01 first = list.getFirst();
+            if (first != null) {
+                System.out.println("Data Mahasiswa Terdepan: ");
+                first.tampil();
+            }
+            System.out.println();
+            Mahasiswa01 last = list.getLast();
+            if (last != null) {
+                System.out.println("Data Mahasiswa Terakhir: ");
+                last.tampil();
+            }
+            System.out.println();
+            int indeks = -1;
+            System.out.print("Masukkan Indeks: ");
+            indeks = scan.nextInt();
+            scan.nextLine();
+            Mahasiswa01 index = list.getIndex(indeks);
+            System.out.println("Data Indeks ke-" + indeks);
+            index.tampil();
+        }
+        case 12 -> {
+            System.out.println("Jumlah Data pada Double Linked List: " + list.getSize());
+        }
         case 0 -> System.out.println("Keluar dari program.");
         default -> System.out.println("Pilihan tidak valid!");
         }
     } while (pilihan != 0);
         scan.close();
-}
+    }
 }
